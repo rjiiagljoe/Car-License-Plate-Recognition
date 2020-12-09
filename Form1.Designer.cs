d5be3560ac88e3ec6f8989647e41c9ca17b80799
rjiiagl <rjiiagl@gmail.com>
2020-12-09 19:27:25 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace 車牌辨識
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rGBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rGLowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.binaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.negativeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.outlineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.targetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.redToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.rGBToolStripMenuItem,
+            this.rGLowToolStripMenuItem,
+            this.binaryToolStripMenuItem,
+            this.negativeToolStripMenuItem,
+            this.grayToolStripMenuItem,
+            this.aveToolStripMenuItem,
+            this.outlineToolStripMenuItem,
+            this.saveImageToolStripMenuItem,
+            this.targetsToolStripMenuItem,
+            this.filterToolStripMenuItem,
+            this.sortToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(958, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // rGBToolStripMenuItem
+            // 
+            this.rGBToolStripMenuItem.Name = "rGBToolStripMenuItem";
+            this.rGBToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.rGBToolStripMenuItem.Text = "RGB";
+            this.rGBToolStripMenuItem.Click += new System.EventHandler(this.rGBToolStripMenuItem_Click);
+            // 
+            // rGLowToolStripMenuItem
+            // 
+            this.rGLowToolStripMenuItem.Name = "rGLowToolStripMenuItem";
+            this.rGLowToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.rGLowToolStripMenuItem.Text = "RG Low";
+            this.rGLowToolStripMenuItem.Click += new System.EventHandler(this.rGLowToolStripMenuItem_Click);
+            // 
+            // binaryToolStripMenuItem
+            // 
+            this.binaryToolStripMenuItem.Name = "binaryToolStripMenuItem";
+            this.binaryToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.binaryToolStripMenuItem.Text = "Binary";
+            this.binaryToolStripMenuItem.Click += new System.EventHandler(this.binaryToolStripMenuItem_Click);
+            // 
+            // negativeToolStripMenuItem
+            // 
+            this.negativeToolStripMenuItem.Name = "negativeToolStripMenuItem";
+            this.negativeToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.negativeToolStripMenuItem.Text = "Negative";
+            this.negativeToolStripMenuItem.Click += new System.EventHandler(this.negativeToolStripMenuItem_Click);
+            // 
+            // grayToolStripMenuItem
+            // 
+            this.grayToolStripMenuItem.Name = "grayToolStripMenuItem";
+            this.grayToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.grayToolStripMenuItem.Text = "Gray";
+            this.grayToolStripMenuItem.Click += new System.EventHandler(this.grayToolStripMenuItem_Click);
+            // 
+            // aveToolStripMenuItem
+            // 
+            this.aveToolStripMenuItem.Name = "aveToolStripMenuItem";
+            this.aveToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.aveToolStripMenuItem.Text = "Ave";
+            this.aveToolStripMenuItem.Click += new System.EventHandler(this.aveToolStripMenuItem_Click);
+            // 
+            // outlineToolStripMenuItem
+            // 
+            this.outlineToolStripMenuItem.Name = "outlineToolStripMenuItem";
+            this.outlineToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.outlineToolStripMenuItem.Text = "Outline";
+            this.outlineToolStripMenuItem.Click += new System.EventHandler(this.outlineToolStripMenuItem_Click);
+            // 
+            // saveImageToolStripMenuItem
+            // 
+            this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.saveImageToolStripMenuItem.Text = "Save Image";
+            this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.saveImageToolStripMenuItem_Click);
+            // 
+            // targetsToolStripMenuItem
+            // 
+            this.targetsToolStripMenuItem.Name = "targetsToolStripMenuItem";
+            this.targetsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.targetsToolStripMenuItem.Text = "Targets";
+            this.targetsToolStripMenuItem.Click += new System.EventHandler(this.targetsToolStripMenuItem_Click);
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.filterToolStripMenuItem.Text = "Filter";
+            this.filterToolStripMenuItem.Click += new System.EventHandler(this.filterToolStripMenuItem_Click);
+            // 
+            // sortToolStripMenuItem
+            // 
+            this.sortToolStripMenuItem.Name = "sortToolStripMenuItem";
+            this.sortToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.sortToolStripMenuItem.Text = "Sort";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(788, 422);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(958, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rGBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rGLowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem binaryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem negativeToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem grayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem outlineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem targetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 車牌辨識
+{
+    public partial class Form1 : Form
+    {
+        FastPixel f = new FastPixel();//宣告快速繪圖物件
+        int BlockBrightnessDimension = 10;//計算區域亮度區塊的寬與高
+        int[,] BrightnessThreshold;//每一區塊的平均亮度，二值化門檻值
+        int minHeight = 16;
+        int maxHeight = 100;
+        int minWidth = 2;
+        int maxWidth = 100;
+        byte[,] arrayGray;//灰階陣列
+        byte[,] arrayBinary;//二值化陣列
+        byte[,] arrayOutline;//輪廓線陣列
+        ArrayList target_Collection;//目標物件集合
+        Bitmap basemap_Copy;//底圖副本
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //開啟檔案
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+                f.BMP2RGB(bmp);//讀取RGB亮度陣列
+                arrayGray = f.arrayG;
+                pictureBox1.Image = bmp;
+
+            }
+        }
+        //以紅光為灰階
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = f.GrayImg(f.arrayR);
+        }
+        //以綠光為灰階
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = f.GrayImg(f.arrayG);
+        }
+        //以藍光為灰階
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = f.GrayImg(f.arrayB);
+        }
+        //以RGB整合亮度為灰階
+        private void rGBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[,] A = new byte[f.imagewidth, f.imageheight];
+            for (int j=0;j<f.imageheight; j++)
+            {
+                for(int i = 0; i < f.imagewidth; i++)
+                {
+                    byte gray = (byte)(f.arrayR[i,j] * 0.299 + f.arrayG[i,j] * 0.587 + f.arrayB[i,j] * 0.114);
+                    A[i, j] = gray;
+                }
+            }
+            pictureBox1.Image = f.GrayImg(A);
+        }
+        //選擇紅綠光之較暗亮度為灰階
+        private void rGLowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[,] A = new byte[f.imagewidth, f.imageheight];
+            for (int j = 0; j < f.imageheight; j++)
+            {
+                for(int i = 0; i < f.imagewidth; i++)
+                {
+                    if(f.arrayR[i,j] > f.arrayG[i,j])
+                    {
+                        A[i, j] = f.arrayG[i, j];
+                    }
+                    else
+                    {
+                        A[i, j] = f.arrayR[i, j];
+                    }
+                }
+            }
+            pictureBox1.Image = f.GrayImg(A);
+        }
+        //二值化
+        private void binaryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BrightnessThreshold = ThresholdBuild(arrayGray);//門檻值陣列建立
+            arrayBinary = new byte[f.imagewidth, f.imageheight];
+            for(int i=1; i < f.imagewidth-1; i++)
+            {
+                int x = i / BlockBrightnessDimension;
+                for(int j = 1; j < f.imageheight-1; j++)
+                {
+                    int y = j / BlockBrightnessDimension;
+                    if (f.arrayG[i, j] < BrightnessThreshold[x,y])
+                    {
+                        arrayBinary[i, j] = 1;
+                    }
+                }
+            }
+            pictureBox1.Image = f.BWImg(arrayBinary);
+        }
+        //負片
+        private void negativeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[,] A = new byte[f.imagewidth, f.imageheight];
+            for(int j = 0; j < f.imageheight; j++)
+            {
+                for(int i = 0; i < f.imagewidth; i++)
+                {
+                    A[i, j] = (byte)(255 - f.arrayG[i, j]);
+                }
+            }
+            pictureBox1.Image = f.GrayImg(A);
+        }
+        //儲存現狀影像
+        private void saveImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void grayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = f.GrayImg(f.arrayG);
+        }
+
+        //平均亮度方塊圖
+        private void aveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int kx = f.imagewidth / BlockBrightnessDimension;
+            int ky = f.imageheight / BlockBrightnessDimension;
+            BrightnessThreshold = new int[kx, ky];//區塊陣列
+            //累計各區塊亮度值總和
+            for(int i=0;i<f.imagewidth;i++)
+            {
+                int x = i / BlockBrightnessDimension;
+                for(int j = 0; j < f.imageheight; j++)
+                {
+                    int y = j / BlockBrightnessDimension;
+                    BrightnessThreshold[x,y] += f.arrayG[i, j];
+                }
+            }
+            //建立亮度塊狀圖
+            byte[,] A = new byte[f.imagewidth, f.imageheight];
+            for(int i = 0; i < kx; i++) 
+            {
+                for(int j = 0; j < ky; j++)
+                {
+                    BrightnessThreshold[i, j] /= BlockBrightnessDimension*BlockBrightnessDimension;
+                    for(int ii = 0; ii < BlockBrightnessDimension; ii++)
+                    {
+                        for(int jj = 0; jj < BlockBrightnessDimension; jj++)
+                        {
+                            A[i * BlockBrightnessDimension + ii, j * BlockBrightnessDimension + jj] = (byte)BrightnessThreshold[i, j];
+                        }
+                    }
+                }
+            }
+            pictureBox1.Image = f.GrayImg(A);
+
+        }
+
+        //建立輪廓點陣列
+        private byte[,] Outline(byte[,]b)
+        {
+            byte[,] arrayOutline = new byte[f.imagewidth, f.imageheight];
+            for(int i = 1; i < f.imagewidth - 1; i++)
+            {
+                for(int j = 1; j < f.imageheight - 1; j++)
+                {
+                    if (b[i, j] == 0) continue;
+                    if (b[i - 1, j] == 0) { arrayOutline[i, j] = 1;continue; }
+                    if (b[i + 1, j] == 0) { arrayOutline[i, j] = 1;continue; }
+                    if (b[i, j - 1] == 0) { arrayOutline[i, j] = 1;continue; }
+                    if (b[i, j + 1] == 0) { arrayOutline[i, j] = 1; }
+                }
+            }
+            return arrayOutline;
+        }
+
+        //輪廓線
+        private void outlineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            arrayOutline = Outline(arrayBinary);
+            pictureBox1.Image = f.BWImg(arrayOutline);
+        }
+        //選擇顯示某目標之輪廓線
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (arrayOutline == null) return;
+                int x = e.X;
+                int y = e.Y;
+                //尋找左方最近之輪廓點
+                while(arrayOutline[x,y]==0 && x>0)
+                {
+                    x--;
+                }
+                ArrayList A = getGrp(arrayOutline, x, y);//搜尋此目標所有輪廓點
+                Bitmap bmp = f.BWImg(arrayOutline);//建立輪廓圖
+                for(int k=0;k<A.Count;k++)
+                {
+                    Point p = (Point)A[k];
+                    bmp.SetPixel(p.X, p.Y, Color.Red);
+                }
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        //氾濫式演算法取得某目標之輪廓點
+        private ArrayList getGrp(byte[,] q,int i,int j)
+        {
+            if (q[i, j] == 0) return new ArrayList();
+            byte[,] b = (byte[,])q.Clone();//建立輪廓點陣列副本
+            ArrayList nc = new ArrayList();//每一輪搜尋的起點集合
+            nc.Add(new Point(i, j));//輸入之搜尋起點
+            b[i, j] = 0;//清除此起點之輪廓點標記
+            ArrayList A = nc;//此目標中所有目標點的集合
+            do
+            {
+                ArrayList nb = (ArrayList)nc.Clone();//複製此輪之搜尋起點集合
+                nc = new ArrayList();//清除準備蒐集下一輪搜尋起點之集合
+                for (int m = 0; m < nb.Count; m++)
+                {
+                    Point p = (Point)nb[m];//搜尋起點
+                    //在此點周邊3*3區域內找輪廓點
+                    for (int ii = p.X - 1; ii <= p.X + 1; ii++)
+                    {
+                        for (int jj = p.Y - 1; jj <= p.Y + 1; jj++)
+                        {
+                            if (b[ii, jj] == 0) continue;//非輪廓點忽略
+                            Point k = new Point(ii, jj);//建立點物件
+                            nc.Add(k);//本輪搜尋新增的輪廓點
+                            A.Add(k);//加入所有已蒐集到的目標點集合
+                            b[ii, jj] = 0;//清除輪廓點點標記
+                        }
+                    }
+                }
+            } while (nc.Count > 0);//此輪搜尋有新發現輪廓點時繼續搜尋
+            return A;//目標物件集合
+        }
+
+        //門檻值陣列建立
+        private int[,] ThresholdBuild(byte[,]b)
+        {
+            int kx = f.imagewidth / BlockBrightnessDimension, ky = f.imageheight / BlockBrightnessDimension;
+            BrightnessThreshold = new int[kx, ky];
+            //累計各區塊亮度值總和
+            for(int i=0;i<f.imagewidth;i++)
+            {
+                int x = i / BlockBrightnessDimension;
+                for(int j=0;j<f.imageheight;j++)
+                {
+                    int y = j / BlockBrightnessDimension;
+                    BrightnessThreshold[x, y] += f.arrayG[i, j];
+                }
+            }
+            for(int i=0;i<kx;i++)
+            {
+                for(int j=0;j<ky;j++)
+                {
+                    BrightnessThreshold[i, j] /= BlockBrightnessDimension * BlockBrightnessDimension;
+                }
+            }
+            return BrightnessThreshold;
+        }
+
+        //以輪廓點建立目標陣列，排除負目標
+        private ArrayList getTargets(byte[,]q)
+        {
+            ArrayList A = new ArrayList();
+            byte[,] b = (byte[,])q.Clone();//建立輪廓點陣列副本
+            for(int i = 1; i < f.imagewidth - 1; i++)
+            {
+                for(int j = 1; j < f.imageheight - 1; j++)
+                {
+                    if (b[i, j] == 0) continue;
+                    TgInfo G = new TgInfo();
+                    G.x_max_negative = i;
+                    G.x_max_positive = i;
+                    G.y_max_negative = j;
+                    G.y_max_positive = j;
+                    G.targetPointList = new ArrayList();
+                    ArrayList nc = new ArrayList();//每一輪搜尋的起點集合
+                    nc.Add(new Point(i, j));//輸入之搜尋起點
+                    G.targetPointList.Add(new Point(i, j));
+                    b[i, j] = 0;//清除此起點之輪廓點標記
+                    do
+                    {
+                        ArrayList nb = (ArrayList)nc.Clone();//複製此輪之搜尋點集合
+                        nc = new ArrayList();//清除準備蒐集下一輪搜尋起點之集合
+                        for (int m = 0; m < nb.Count; m++)
+                        {
+                            Point p = (Point)nb[m];//搜尋起點
+                            //在此點周邊3*3區域內找輪廓點
+                            for (int ii = p.X - 1; ii <= p.X + 1; ii++)
+                            {
+                                for (int jj = p.Y - 1; jj <= p.Y + 1; jj++)
+                                {
+                                    if (b[ii, jj] == 0) continue;//非輪廓點忽略
+                                    Point k = new Point(ii, jj);//建立點物件
+                                    nc.Add(k);//本輪搜尋新增的輪廓點
+                                    G.targetPointList.Add(k);
+                                    G.targetPoint += 1;//點數累計
+                                    if (ii < G.x_max_negative) G.x_max_negative = ii;
+                                    if (ii > G.x_max_positive) G.x_max_positive = ii;
+                                    if (jj < G.y_max_negative) G.y_max_negative = jj;
+                                    if (jj > G.y_max_positive) G.y_max_positive = jj;
+                                    b[ii, jj] = 0;//清除輪廓點點標記
+                                }
+                            }
+                        }
+                    } while (nc.Count > 0);//此輪搜尋有新發現輪廓點時繼續搜尋
+                    if (arrayBinary[i - 1, j] == 1) continue;//排除白色區塊的負目標，起點左邊是黑點
+                    G.width = G.x_max_positive - G.x_max_negative + 1;//寬度計算
+                    G.height = G.y_max_positive - G.y_max_negative + 1;//高度計算
+                    A.Add(G);//加入有效目標集合
+                }
+            }
+            return A;//回傳目標物件集合
+        }
+
+        //建立目標物件
+        private void targetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            target_Collection = getTargets(arrayOutline);//建立目標物件集合
+            //繪製目標輪廓點
+            Bitmap bmp = new Bitmap(f.imagewidth, f.imageheight);
+            for(int k = 0; k < target_Collection.Count - 1; k++)
+            {
+                TgInfo T = (TgInfo)target_Collection[k];
+                for(int m = 0; m < T.targetPointList.Count; m++)
+                {
+                    Point p = (Point)T.targetPointList[m];
+                    bmp.SetPixel(p.X, p.Y, Color.Black);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        //依據目標大小篩選目標
+        private void filterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ArrayList D = new ArrayList();
+            for(int k = 0; k < target_Collection.Count; k++)
+            {
+                TgInfo T = (TgInfo)target_Collection[k];
+                if (T.height < minHeight) continue;
+                if (T.height > maxHeight) continue;
+                if (T.width < minWidth) continue;
+                if (T.width > maxWidth) continue;
+                D.Add(T);
+            }
+            target_Collection = D;
+            //繪製有效目標
+            Bitmap bmp = new Bitmap(f.imagewidth, f.imageheight);
+            for(int k = 0; k < target_Collection.Count - 1; k++)
+            {
+                TgInfo T = (TgInfo)target_Collection[k];
+                for(int m = 0; m < T.targetPointList.Count; m++)
+                {
+                    Point p = (Point)T.targetPointList[m];
+                    bmp.SetPixel(p.X, p.Y, Color.Black);
+                }
+            }
+            pictureBox1.Image = bmp;
+            basemap_Copy = (Bitmap)bmp.Clone();
+        }
+    }
+}
